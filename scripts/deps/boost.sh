67e9a8e2b69604dcb18937519364d949a78b8f06
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-09-21 06:23:34 +0300

    fix boost.sh

--- scripts/deps/boost.sh
+++ scripts/deps/boost.sh
@@ -6,4 +6,4 @@ git submodule update --init tools/boostdep tools/build
 git submodule update --init libs/test
 python tools/boostdep/depinst/depinst.py test
 ./bootstrap.sh gcc
-./b2 -j4 --with-test --build-type=minimal variant=release link=dynamic --toolset=gcc address-model=64 stage --layout=system
+./b2 -j4 --with-test --build-type=minimal variant=release link=shared --toolset=gcc address-model=64 stage --layout=system
